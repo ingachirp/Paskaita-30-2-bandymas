ffba734680edb5ac25209365135261d57797575b
I.G <ingrida.gudaityte@gmail.com>
2022-01-22 12:12:52 +0200

    30 p su 3-iem uzduotim

---
+++ js30.js
@@ -0,0 +1,25 @@
+function min(a, b){
+    if a<b {
+        return a;
+    }
+    return b;
+}
+const atsakymas = min (4,7);
+
+console.log(atsakymas)
+// 3 uzduotis is skaidriu
+
+function sayHi() {
+    alert("Sveiki!");
+}
+sayHi();
+
+// const wowString = 'iterptas kintamasis';
+// `labas ${wowString}`;
+
+function sayHi(word) {
+    alert("Sveiki!" + word);
+}
+
+sayHi();
+
