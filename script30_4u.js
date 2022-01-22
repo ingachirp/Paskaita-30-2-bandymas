4adda5fdc52aa830b1c0789b21ed8cd281b960f1
I.G <ingrida.gudaityte@gmail.com>
2022-01-22 14:45:40 +0200

    6  uzduotis

--- script30_4u.js
+++ script30_4u.js
@@ -1,5 +1,25 @@
-// 5 uzduotis
-function suma(a,b) {
-    return a + b;
+// 6 uzduotis vertimas Celsijus i Farengeita
+const celsijus = prompt('Įveskite laipsnius: ');
+const atsakymas = cToF(celsijus);
+alert(atsakymas);
+
+function cToF(celsijus) {
+    return celsijus * 1.8 + 32;
 }
-alert(suma(5, 7));
+// const ats1 = cToF(-5);
+// const ats2 = cToF(0);
+// const ats3 = cToF(30);
+// const ats4 = cToF(2);
+
+// alert(ats1);
+// alert(ats2);
+// alert(ats3);
+// alert(ats4);
+
+const eur = prompt('Įveskite euro kiekį: ');
+const atsakymas2 = eurToRub(eur);
+alert(atsakymas2);
+
+function eurToRub(eur) {
+    return eur * 88.05;
+}
